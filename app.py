c18a97dbe17b684a27533c6cc08f707395525517
wackloner <wackloner@gmail.com>
2022-04-19 20:09:17 +0400

    add dao domain to cors policy

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=['*'],
+    allow_origins=[
+        'https://app.cometa.farm/meta-dao',
+        '*'
+    ],
     allow_methods=['*'],
     allow_headers=['*'],
 )
