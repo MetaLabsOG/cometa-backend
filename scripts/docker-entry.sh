43b1e80d3c065d5bb8ec69dc84730a0e9f4c370c
wackloner <wackloner@gmail.com>
2022-04-21 14:19:16 +0400

    remove certs from uvicorn

--- scripts/docker-entry.sh
+++ scripts/docker-entry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uvicorn app:app --host 0.0.0.0 --port 5000 --ssl-keyfile ./key.pem --ssl-certfile ./cert.pem # TODO: --workers "$WORKERS_NUM"
+uvicorn app:app --host 0.0.0.0 --port 5000 # TODO: --workers "$WORKERS_NUM"
