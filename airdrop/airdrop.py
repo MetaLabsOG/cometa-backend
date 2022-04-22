0f6f4533fb4b5cfa04ac1f70f27b7064ef290030
wackloner <wackloner@gmail.com>
2022-04-22 14:35:31 +0400

    airdrop send tokens

---
+++ airdrop/airdrop.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+
+from algosdk import mnemonic, account
+from algosdk.future import transaction
+from algosdk.v2client import algod
+from pymongo import MongoClient
+
+from env import MONGO_PORT, DB_NAME, settings
+
+TOTAL_SUPPLY = 1000000
+TOTAL_AIRDROPS = 12
+TOTAL_PERCENT = 0.03
+
+META_ASA_ID = 123  # TODO
+
+CURRENT_PERCENT = TOTAL_PERCENT / TOTAL_AIRDROPS
+CURRENT_SUPPLY = TOTAL_SUPPLY * CURRENT_PERCENT
+
+
+db = MongoClient(port=MONGO_PORT)[DB_NAME]
+
+algod_client = algod.AlgodClient(settings.algod_token, settings.algod_address,
+                                 headers={
+                                     'User-Agent': 'py-algorand-sdk',
+                                     'X-API-Key': settings.algod_token
+                                 })
+private_key = mnemonic.to_private_key(settings.algo_mnemonic)
+public_key = account.address_from_private_key(private_key)
+
+
+def send_tokens(address: str, amount: float, airdrop_id: str):
+    done = db.airdrops.find_one({'airdrop_id': airdrop_id, 'address': address})
+    if done is not None:
+        print(f'Already sent {amount} to {address} before!')
+        return
+
+    print(f'Sending {amount} META to {address}!')
+    params = algod_client.suggested_params()
+
+    unsigned_txn = transaction.AssetTransferTxn(
+        sender=public_key,
+        sp=params,
+        receiver=address,
+        amt=amount,
+        index=META_ASA_ID,
+        note=f'Metapunks Airdrop #{airdrop_id}'
+    )
+    signed_txn = unsigned_txn.sign(private_key)
+    txid = algod_client.send_transaction(signed_txn)
+
+    db.airdrops.insert_one(
+        {
+            'airdrop_id': airdrop_id,
+            'address': address,
+            'amount': amount,
+            'txid': txid,
+            'time': datetime.now()
+        }
+    )
+
+    transaction.wait_for_confirmation(algod_client, txid, 4)
+
+    print(f'Sent with {txid}!')
+
+
+def run(airdrop_id: str):
+    print(f'Airdrop #{airdrop_id} in progress!')
+
+    snapshot = db.snapshots.find_one({'snapshot_id': airdrop_id})
+    nft_count = snapshot['nft_count']
+    print(f'Total {nft_count} NFTs!')
+
+    tokens_per_nft = CURRENT_SUPPLY / nft_count
+    print(f'{tokens_per_nft} META per NFT!')
+
+    amount_sent = 0
+
+    for holder in snapshot['holders']:
+        amount = tokens_per_nft * len(holder['asa_ids'])
+        send_tokens(holder['address'], amount, airdrop_id)
+        amount_sent += amount
+
+    print(f'Sent {amount_sent} tokens!')
+
--- env.py
+++ env.py
@@ -1,14 +1,44 @@
+from functools import cached_property
+
+from pydantic import BaseSettings
+
 NODE_MODE = "TESTNET"
 
-
-def is_mainnet() -> bool:
-    return NODE_MODE == 'MAINNET'
-
+# TODO: move all to Settings
+MONGO_PORT = 27017
+DB_NAME = NODE_MODE
 
 MICROALGOS_IN_ALGO = 1000000
 
 META_ADDRESSES = [
-  'METAGTX4BELE3WVMF5GUOYZMCDYFMDEKBWBP6VLDF6AKTNFWJSGKUFDAYU',
-  'METASWXOZB3CFFNWD6BDWU7CG5E42HNWFJZMM6IWR7MCT4P7NDW6755IMM',
-  'METAGLOPQRWQFZVA5Q2CFSVXEBPGWW4AUHZTC6B2ZQ6UQW24PS5JAMLQSY'
+    'METAGTX4BELE3WVMF5GUOYZMCDYFMDEKBWBP6VLDF6AKTNFWJSGKUFDAYU',
+    'METASWXOZB3CFFNWD6BDWU7CG5E42HNWFJZMM6IWR7MCT4P7NDW6755IMM',
+    'METAGLOPQRWQFZVA5Q2CFSVXEBPGWW4AUHZTC6B2ZQ6UQW24PS5JAMLQSY',
+    'METAEVEML4X7TXWHCBP4TKJDUZ7X2O7MSRECM57YA5TPFYSAI6J7WKCX3E', # Custom
+    'METAUPN7HLU67ASI4YBYX3BWZEXNYL2CQWGCZES2DM7AGXAHUDZQ2LZMEY'  # Legendary
 ]
+
+
+class Settings(BaseSettings):
+    algo_network: str
+    algo_mnemonic: str
+
+    testnet_algod_address: str
+    mainnet_algod_address: str
+
+    algod_token: str
+
+    class Config:
+        env_file = '.env'
+        arbitrary_types_allowed = True
+        keep_untouched = (cached_property,)
+
+    def is_mainnet(self):
+        return self.algo_network == 'MAINNET'
+
+    @cached_property
+    def algod_address(self):
+        return self.mainnet_algod_address if self.is_mainnet() else self.testnet_algod_address
+
+
+settings = Settings()
