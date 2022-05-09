785af0031ef90425bc9001ac008fc49ee140697d
wackloner <wackloner@gmail.com>
2022-05-09 15:05:41 +0400

    stabilize DAO asset compatibility

--- api/wallet_manager.py
+++ api/wallet_manager.py
@@ -28,8 +28,9 @@ def get_wallet_assets(address: str) -> List[AssetInfo]:
     DEFLY_IN_ALGO = 0.0088
     return [
         AssetInfo('USD Coin', 'USDC', 1589, Price(1, int(1 / algo_price * MICROALGOS_IN_ALGO))),
-        AssetInfo('Algorand', 'ALGO', 12625, Price(algo_price, MICROALGOS_IN_ALGO)),
-        AssetInfo('gALGO3', 'gALGO3', 1125, Price(algo_price, MICROALGOS_IN_ALGO)),
+        AssetInfo('Algorand', 'ALGO', 13750, Price(algo_price, MICROALGOS_IN_ALGO)),  # TODO: next two lines insted of this when front is fixed
+        # AssetInfo('Algorand', 'ALGO', 12625, Price(algo_price, MICROALGOS_IN_ALGO)),
+        # AssetInfo('gALGO3', 'gALGO3', 1125, Price(algo_price, MICROALGOS_IN_ALGO)),
         AssetInfo('Defly Token', 'DEFLY', 24672, Price(algo_price * DEFLY_IN_ALGO, DEFLY_IN_ALGO * MICROALGOS_IN_ALGO)),
     ]
 
