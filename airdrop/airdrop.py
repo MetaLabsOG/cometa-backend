9139945a756025df1cc19fa2449699aa0474d327
wackloner <wackloner@gmail.com>
2022-05-15 17:35:32 +0400

    count only opted-in holders for the airdrop

--- airdrop/airdrop.py
+++ airdrop/airdrop.py
@@ -6,6 +6,7 @@ from algosdk.v2client import algod
 from pymongo import MongoClient
 
 from blockchain.assets import META_TOTAL_SUPPLY, META_ASA_ID, META_DECIMALS
+from dexes.tinyman import tinyman_from_algod
 from env import settings
 
 TOTAL_AIRDROPS = 12
@@ -22,15 +23,16 @@ algod_client = algod.AlgodClient(settings.algod_token, settings.algod_address,
                                      'User-Agent': 'py-algorand-sdk',
                                      'X-API-Key': settings.algod_token
                                  })
+tinyman_client = tinyman_from_algod(algod_client)
 private_key = mnemonic.to_private_key(settings.algo_mnemonic)
 public_key = account.address_from_private_key(private_key)
 
 
-def send_tokens(address: str, amount: float, airdrop_id: str):
+def send_tokens(address: str, amount: float, airdrop_id: str) -> float:
     done = db.airdrops.find_one({'airdrop_id': airdrop_id, 'address': address})
     if done is not None:
         print(f'Already sent {amount} to {address} before!')
-        return
+        return 0
 
     print(f'Sending {amount} META to {address}!')
     amount_micros = int(amount * (10 ** META_DECIMALS))
@@ -61,6 +63,23 @@ def send_tokens(address: str, amount: float, airdrop_id: str):
 
     print(f'Sent with {txid}!')
 
+    return amount
+
+
+def filter_opted_in(airdrop_id: str, holders):
+    res = []
+    for holder in holders:
+        address = holder['address']
+        if tinyman_client.asset_is_opted_in(META_ASA_ID, address):
+            record = {
+                'airdrop_id': airdrop_id,
+                'address': address,
+                'nft_cnt': len(holder['asa_ids'])
+            }
+            res.append(record)
+            db.opted_in.insert_one(record)
+    return res
+
 
 def run(airdrop_id: str):
     print(f'Airdrop #{airdrop_id} in progress!')
@@ -73,17 +92,25 @@ def run(airdrop_id: str):
     holders = snapshot['holders']
     print(f'Total {len(holders)} holders!')
 
-    tokens_per_nft = AIRDROP_SUPPLY / nft_count
+    opted_in = filter_opted_in(airdrop_id, holders)
+    print(f'Total {len(opted_in)} holders are opted-in!')
+
+    total_nfts = 0
+    for holder in opted_in:
+        total_nfts += holder['nft_cnt']
+    print(f'Total {total_nfts} opted-in NFTs!')
+
+    tokens_per_nft = AIRDROP_SUPPLY / total_nfts
     print(f'Total {AIRDROP_SUPPLY} tokens, {tokens_per_nft} META per NFT!\n')
 
     amount_sent = 0
     holders_sent = 0
 
-    for holder in holders:
-        amount = tokens_per_nft * len(holder['asa_ids'])
+    for holder in opted_in:
+        amount = tokens_per_nft * holder['nft_cnt']
         address = holder['address']
         try:
-            send_tokens(address, amount, airdrop_id)
+            amount = send_tokens(address, amount, airdrop_id)
             amount_sent += amount
             holders_sent += 1
             print(f'Sent {amount} to {address}')
