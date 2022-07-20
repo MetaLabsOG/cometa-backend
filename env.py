ed1af794d219825780c23e301a2655137bcb8480
wackloner <wackloner@gmail.com>
2022-07-20 23:12:41 +0300

    fix docker-compose.yml

--- env.py
+++ env.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     algod_token: str
 
     server_port: int
-    mongodb_host: str = 'localhost'
+    mongodb_host: str
     mongodb_port: int
 
     api_password: str
