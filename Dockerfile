30376702112e78c8710b28372943abd659c40e58
wackloner <wackloner@gmail.com>
2022-07-20 23:01:45 +0300

    add mongodb to docker-compose, add a few helper scripts

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ FROM nikolaik/python-nodejs:python3.9-nodejs16-bullseye
 COPY . /app
 WORKDIR /app
 
-# RUN pip install pipenv
 RUN pipenv install --deploy
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,4 +2,4 @@
 
 ENV=${1:-testnet}
 
-docker-compose -f "docker-compose.$ENV.yml" down && docker-compose -f "docker-compose.$ENV.yml" up -d
+scripts/stop.sh "$@" && scripts/start.sh "$@"
