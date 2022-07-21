1f49b67ed3262cf24fa8d8a370821acbfc4ea6a7
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2022-07-21 14:02:16 +0000

    fix farm pkg version and broken env vars parsing

--- js/config.mjs
+++ js/config.mjs
@@ -1,5 +1,5 @@
-export const COMETA_ENV = process.env.COMETA_ENVIRONMENT || "test";
-export const NETWORK = COMETA_ENV === "test" ? "TestNet" : "MainNet";
+export const COMETA_ENV = process.env.COMETA_ENVIRONMENT || "testnet";
+export const NETWORK = COMETA_ENV === "testnet" ? "TestNet" : "MainNet";
 
 export const META_ASA_IDS = {
   MainNet: 712012773,
@@ -8,4 +8,3 @@ export const META_ASA_IDS = {
 
 export const META_TOKEN = META_ASA_IDS[NETWORK];
 export const MNEMONIC = process.env.DEPLOY_MNEMONIC;
-
