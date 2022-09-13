ebc2f3ac577e551f20ed6f33f681f4dc78105d85
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2022-09-13 13:25:20 +0000

    add fetching humble pools to mongodb

---
+++ dexes/humble.py
@@ -0,0 +1,30 @@
+from typing import List, Optional
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+from core import mongodb
+
+humble_pools = mongodb.database.humblePools
+
+@dataclass_json
+@dataclass
+class HumblePool:
+    poolAddress: int
+    poolTokenId: int
+    mintedLiquidityTokens: int
+    n2nn: bool
+    tokenAId: int
+    tokenABalance: str
+    tokenAFees: str
+    tokenADecimals: int
+    tokenBId: int
+    tokenBBalance: str
+    tokenBFees: str
+    tokenBDecimals: int
+
+def get_pool_by_id(pool_id: int) -> Optional[HumblePool]:
+    res = humble_pools.find_one({'poolAddress': pool_id })
+    return HumblePool.from_dict(res) if res else None
+
+def get_pools_by_assets(assetA: int, assetB: int) -> List[HumblePool]:
+    return list(map(HumblePool.from_dict, humble_pools.find({ 'tokenAId': assetA, 'tokenBId': assetB })))
