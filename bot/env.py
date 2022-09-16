827bb92a64c28e4975163f56fdcd88e5a6e10580
wackloner <wackloner@gmail.com>
2022-09-17 04:25:56 +0800

    notifications delay update

--- bot/env.py
+++ bot/env.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 
 # TODO: compute for each pool/user
 BEST_COMPOUNDING_DELAY = timedelta(minutes=5)
-REMIND_AGAIN_DELAY = timedelta(minutes=2)
+REMIND_AGAIN_DELAY = timedelta(minutes=1340)
 
 MONITOR_LOG_DELAY = timedelta(seconds=10)
 AIRTABLE_UPDATE_DELAY_SECONDS = 30
