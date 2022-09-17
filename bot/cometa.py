55077b9c880ae441e6564fa2bc96883e7b76c8bc
wackloner <wackloner@gmail.com>
2022-09-17 17:09:44 +0800

    recalculate rewards correctly

--- bot/cometa.py
+++ bot/cometa.py
@@ -1,13 +1,12 @@
 import logging
 import time
 from dataclasses import dataclass
-from threading import Thread
 from typing import Optional, List
 
 import schedule
 from pyairtable import Base
 
-from api.stats import get_pool_state
+from api.stats import get_pool_state, PoolState
 from blockchain.indexer import get_asset
 from blockchain.node import get_current_round
 from bot.db import events, users
@@ -35,10 +34,10 @@ class UserPool:
     ended_duration: Optional[float]
 
 
-async def get_local_states(type: str, address: str):
+async def get_local_states(type: str, address: str) -> dict:
     contracts = get_contracts({'type': type})
     if not contracts:
-        return []
+        return {}
     ids_and_versions = [{'id': info.id, 'version': strip_version(info.version)} for info in contracts]
     local_states = await calljs("fetchContractsLocalViews",
                                 contractType=type,
@@ -47,17 +46,33 @@ async def get_local_states(type: str, address: str):
     return local_states
 
 
+# ffs
+BIG_NUM = 1000000000000000000
+
+def recalculate_reward(pool: PoolState, current_block: int, staked: int, reward: int,
+                       reward_per_token_paid: int) -> int:
+    if pool.last_update_block >= current_block or pool.total_staked == 0 or staked == 0:
+        return reward
+
+    last_block_with_rewards = min(current_block, pool.end_block)
+    reward_blocks_passed = last_block_with_rewards - pool.last_update_block
+    reward_per_token_stored_new = pool.reward_per_token_stored + \
+                                  reward_blocks_passed * pool.reward_per_block * BIG_NUM // pool.total_staked
+    reward_to_pay_now = staked * (reward_per_token_stored_new - reward_per_token_paid) // BIG_NUM
+    return reward + reward_to_pay_now
+
+
 async def get_user_pools(address: str) -> List[UserPool]:
     local_states = await get_local_states('farm', address) | await get_local_states('distribution', address)
     all_contracts = get_contracts({'type': {'$in': ['farm', 'distribution']}})
     contract_by_id = {str(c.id): c for c in all_contracts}
     pools = []
     for pool_id, state in local_states.items():
-        current_reward = parse_bignum(state['reward'])
+        reward = parse_bignum(state['reward'])
         staked = parse_bignum(state['staked'])
 
         # user doesn't have interest in such pools
-        if current_reward == 0 and staked == 0:
+        if reward == 0 and staked == 0:
             continue
 
         lock_timestamp = parse_bignum(state['lockTimestamp'])
@@ -70,19 +85,27 @@ async def get_user_pools(address: str) -> List[UserPool]:
         if current_block > pool_state.end_block:
             ended_duration = blocks_to_seconds(pool_state.end_block, current_block)
 
-        staked_usd = pool_state.total_cost_usd * staked / pool_state.microtokens_staked
+        staked_usd = pool_state.total_cost_usd * staked / pool_state.total_staked
 
         logger.debug(contract.description)
         logger.debug(contract.id)
+
+        logger.debug(f'reward = {reward}')
+        reward_per_token_paid = parse_bignum(state['rewardPerTokenPaid'])
+        reward = recalculate_reward(pool_state, current_block, staked, reward, reward_per_token_paid)
+        logger.debug(f'new_reward = {reward}')
+
         reward_asset = get_asset(pool_state.reward_token_id)
-        logger.debug(f'current_reward = {current_reward}')
         logger.debug(f'reward_token = {pool_state.reward_token_id}')
-        reward_tokens = current_reward / (10 ** reward_asset['params']['decimals'])
+
+        reward_tokens = reward / (10 ** reward_asset['params']['decimals'])
         logger.debug(f'reward_tokens = {reward_tokens}')
+
         reward_price = get_asset_price(pool_state.reward_token_id)
         reward_usd = reward_tokens * reward_price
         logger.debug(f'reward_usd = {reward_usd}')
-        reward_usd += current_reward  * pool_state.total_algo_rewards * get_algo_price() / pool_state.total_rewards
+
+        reward_usd += reward * pool_state.total_algo_rewards * get_algo_price() / pool_state.total_rewards
         logger.debug(f'reward_usd_with_algo = {reward_usd}\n')
 
         pools.append(UserPool(
@@ -182,6 +205,7 @@ def schedule_airtable_updates():
     # daemon = Thread(target=updater_thread, args=(), daemon=True, name='Updater')
     # daemon.start()
     pass
+
 
 if __name__ == '__main__':
     process_updates()
