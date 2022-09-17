67468723f4566b8b85fd6db62f889459f13e7e44
wackloner <wackloner@gmail.com>
2022-09-17 17:09:44 +0800

    recalculate rewards correctly

--- telegram_bot.py
+++ telegram_bot.py
@@ -58,7 +58,7 @@ async def show_pools(update: Update, context: CallbackContext):
                 reply_text += f'<i>Withdraw ASAP! It ended {seconds_format(pool.ended_duration)}s ago :(</i>\n'
             reply_text += '\n'
 
-        reply_text += '\nTo manage your pools go to https://app.cometa.farm/'
+        reply_text += 'To manage your pools go to https://app.cometa.farm/'
     else:
         reply_text = '🤖 You don\'t have any pools, that\'s strange...' \
                      '\n\n' \
