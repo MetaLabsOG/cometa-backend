066db048a994ea172f6bc1e317beb1823ed629fb
wackloner <wackloner@gmail.com>
2022-09-28 12:27:17 +0800

    fix race condition

--- bot/background.py
+++ bot/background.py
@@ -3,20 +3,30 @@ import logging
 import multiprocessing
 from contextlib import contextmanager
 
-from bot.notifier import notify_all
-from bot.user_pools import update_users_pools
-from core.decorators import repeat_every
+from bot.db import users
+from bot.notifier import notify_user
+from bot.user_pools import update_user_pools
+from core.decorators import repeat_every, safe_async_method
 
 spawn = multiprocessing.get_context('spawn')
 logger = logging.getLogger(__name__)
+
+
+@safe_async_method
+async def update_and_notify():
+    all_users = users.get_users({})
+
+    for user in all_users:
+        await update_user_pools(user)
+        if user.should_remind():
+            await notify_user(user)
 
 
 @repeat_every(60)  # once in a minute
 async def update_users():
     logger.info('Updating users...')
 
-    await update_users_pools()
-    await notify_all()
+    await update_and_notify()
 
 
 # TODO: graceful shutdown here (with signal handling?)
