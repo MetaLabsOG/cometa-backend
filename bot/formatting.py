680a7ddf168aa4fc132fcbd3aa6e182615d8fb2e
wackloner <wackloner@gmail.com>
2022-09-28 15:08:20 +0800

    move user pool info formatting to separate file

--- bot/formatting.py
+++ bot/formatting.py
@@ -1,5 +1,18 @@
+from bot.utils import usd_format, seconds_format
 from core.model import UserPool
 
 
 def format_user_pool(pool: UserPool) -> str:
-    pass
+    if pool.ended_duration is not None:
+        return f'❌<b>{pool.name}.</b>\n' \
+               f'Staked = ${usd_format(pool.staked_usd)}, rewards = ${usd_format(pool.reward_usd)}\n' \
+               f'<i><b>Withdraw</b> ASAP! It ended {seconds_format(pool.ended_duration)} ago :(</i>\n\n'
+
+    farmed_percent = pool.reward_usd / pool.staked_usd * 100
+    text = f'✅<b>{pool.name}</b>, <i>{usd_format(pool.current_apr)}% APR</i>.\n' \
+           f'Staked = ${usd_format(pool.staked_usd)}, rewards = ${usd_format(pool.reward_usd)}\n'
+    if farmed_percent > 1:
+        text += f'<i>You\'ve already farmed <b>{usd_format(farmed_percent)}%</b> from your stake! ' \
+                f'Good time for <b>compounding</b>!</i>\n'
+
+    return text
--- bot/notifier.py
+++ bot/notifier.py
@@ -3,12 +3,12 @@ import time
 
 from telegram.constants import ParseMode
 
+from bot.formatting import format_user_pool
 from bot.user_pools import get_user_pools
 from bot.context import app_context
 from bot.db import users
 from bot.db.model import CometaUser
 from bot.phrase_manager import Phrases
-from bot.utils import seconds_format, usd_format
 from core.decorators import safe_async_method
 
 logger = logging.getLogger(__name__)
@@ -23,24 +23,22 @@ async def notify_user(user: CometaUser):
     ended_pools = list(filter(lambda p: p.ended_duration is not None, pools))
     if ended_pools:
         text += f'You still have stake/reward in <b>{len(ended_pools)} ended</b> pools!\n\n'
-        for pool in ended_pools:
-            text += f'❌<b>{pool.name}</b>\n' \
-                    f'Staked = ${usd_format(pool.staked_usd)}, rewards = ${usd_format(pool.reward_usd)}\n' \
-                    f'<i>It ended {seconds_format(pool.ended_duration)} ago :(</i>\n\n'
+        for pool in ended_pools:  # TODO: use join
+            text += format_user_pool(pool)
+            text += '\n'
 
     live_pools = list(filter(lambda p: p.ended_duration is None, pools))
     compound_pools = []
     for pool in live_pools:
-        percent = pool.reward_usd / pool.staked_usd
-        if percent > 0.01:  # TODO: user setting
+        percent = pool.reward_usd / pool.staked_usd * 100
+        if percent > 1:  # TODO: user setting
             compound_pools.append((pool, percent))
 
     if compound_pools:
         text += f'What about some compounding?😏\n\n'
-        for pool, percent in compound_pools:
-            text += f'✅<b>{pool.name}, {usd_format(pool.current_apr)}% APR.</b>\n' \
-                    f'Staked = ${usd_format(pool.staked_usd)}, rewards = ${usd_format(pool.reward_usd)}\n' \
-                    f'<i>You\'ve already farmed {usd_format(percent * 100)}% from your stake! Good time for compounding!</i>\n\n'
+        for pool, percent in compound_pools:  # TODO: use join
+            text += format_user_pool(pool)
+            text += '\n'
 
     if ended_pools or compound_pools:
         text += 'It is the time.\nhttps://app.cometa.farm/\n\n'
