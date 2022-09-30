f7ad6ef5825c4d9030b9d117a60bf00febf1ccef
wackloner <wackloner@gmail.com>
2022-09-30 14:25:25 +0700

    fix APY

--- bot/formatting.py
+++ bot/formatting.py
@@ -9,15 +9,15 @@ def calculate_apy(apr: float, periods: int) -> float:
 def format_user_pool(pool: UserPool) -> str:
     if pool.ended_duration is not None:
         return f'❌<b>{pool.name}.</b>\n' \
-               f'💸 Staked = ${usd_format(pool.staked_usd)}, rewards = ${usd_format(pool.reward_usd)}\n' \
-               f'<i><b>Withdraw</b> ASAP! It ended {seconds_format(pool.ended_duration)} ago :(</i>\n\n'
+               f'💸Stake = <b>${usd_format(pool.staked_usd)}</b>, rewards = <b>${usd_format(pool.reward_usd)}</b>\n' \
+               f'<i><b>Withdraw</b> ASAP! It ended {seconds_format(pool.ended_duration)} ago :(</i>\n'
 
     farmed_percent = pool.reward_usd / pool.staked_usd * 100
-    text = f'☄️ <b>{pool.name}</b>, <i>{usd_format(pool.current_apr)}% APR</i>.\n' \
-           f'Staked = <b>${usd_format(pool.staked_usd)}</b>, rewards = <b>${usd_format(pool.reward_usd)}</b>\n'
+    text = f'✅<b>{pool.name}</b>, <i>{usd_format(pool.current_apr)}% APR</i>.\n' \
+           f'💸Stake = <b>${usd_format(pool.staked_usd)}</b>, rewards = <b>${usd_format(pool.reward_usd)}</b>\n'
     if farmed_percent > 1:
-        apy = calculate_apy(pool.current_apr, 365)
-        text += f'<i><b>Compound</b> for sure! Already farmed <b>{usd_format(farmed_percent)}%</b> from staked!</i>\n' \
-                f'If compounded <b>daily</b>, you would get <b>{usd_format(apy)}%</b> APY!\n'
+        apy = calculate_apy(pool.current_apr / 100, 365) * 100
+        text += f'<i><b>Compound</b> for sure! Already farmed <b>{usd_format(farmed_percent)}%</b> from stake!</i>\n' \
+                f'<i>If compounded <b>daily</b>, you would get <b>{usd_format(apy)}%</b> APY!</i>\n'
 
     return text
