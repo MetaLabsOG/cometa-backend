46adc7e99b2ecd402b42252fb94da5c0580dd9f4
Nikita Gorokhov <wackloner@gmail.com>
2022-10-15 17:17:33 +0000

    okay caches are fixed... but at what cost...

--- js/commands.mjs
+++ js/commands.mjs
@@ -3,6 +3,10 @@ import semver from "semver";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
 import { loadStdlib } from "@reach-sh/stdlib";
+import algosdk from 'algosdk';
+import * as RHC from '@reach-sh/stdlib/dist/cjs/ALGO_ReachHTTPClient.js';
+import * as UTBC from '@reach-sh/stdlib/dist/cjs/ALGO_UTBC.js';
+
 import { deployStandardContract } from "@metalabsog/common";
 
 import * as crowdsale from "@metalabsog/crowdsale";
@@ -11,7 +15,7 @@ import * as farm_17_2_5 from "metalabsog-farm-17_2_5";
 import * as distribution_17_0_4 from "metalabsog-distribution-17_0_4";
 import * as distribution_17_0_5 from "metalabsog-distribution-17_0_5";
 
-import { NETWORK, MNEMONIC } from "./config.mjs";
+import { NETWORK, MNEMONIC, REACH_ALGO_ENV } from "./config.mjs";
 
 // This module is ES module because our contract modules are ES modules, and we want them
 // to be loaded synchronously.
@@ -22,10 +26,100 @@ const __dirname = path.dirname(__filename);
 // ...and they also cannot import json without webpack...
 const pkg = JSON.parse(readFileSync(path.resolve(__dirname, "./package.json")));
 
+// issue THAT
+//
+const localhostProviderEnv = {
+    ALGO_SERVER: 'http://localhost',
+    ALGO_PORT: '4180',
+    ALGO_TOKEN: 'REMOVED_CREDENTIAL',
+    ALGO_INDEXER_SERVER: 'http://localhost',
+    ALGO_INDEXER_PORT: '8980',
+    ALGO_INDEXER_TOKEN: 'reach-devnet',
+    REACH_ISOLATED_NETWORK: 'yes',
+    ALGO_NODE_WRITE_ONLY: 'no',
+};
+
+function truthyEnv(v) {
+    if (!v) return false;
+    return !['0', 'false', 'f', '#f', 'no', 'off', 'n', ''].includes(v && v.toLowerCase && v.toLowerCase());
+}
+
+function envDefaultsALGO(env) {
+    return { ...localhostProviderEnv, ...env };
+}
+
+// These two functions allow us to use PureStake API with Reach without issue
+export function indexerFromEnv(env) {
+    const { ALGO_INDEXER_SERVER, ALGO_INDEXER_PORT, ALGO_INDEXER_TOKEN } = env;
+    const port = ALGO_INDEXER_PORT || undefined; // UTBC checks for undefined
+    const token =
+        typeof ALGO_INDEXER_TOKEN === 'string' ? { 'X-Indexer-API-Token': ALGO_INDEXER_TOKEN } : ALGO_INDEXER_TOKEN;
+
+    const utbc = new UTBC.URLTokenBaseHTTPClient(token, ALGO_INDEXER_SERVER, port);
+    const rhc = new RHC.ReachHTTPClient(utbc, 'indexer', async (e) => {
+        // Do nothing
+    });
+    return [rhc, new algosdk.Indexer(rhc)];
+}
+
+export function algodClientFromEnv(env) {
+    const { ALGO_SERVER, ALGO_PORT, ALGO_TOKEN } = env;
+    const port = ALGO_PORT || undefined; // UTBC checks for undefiend
+    const token = typeof ALGO_TOKEN === 'string' ? { 'X-Algo-API-Token': ALGO_TOKEN } : ALGO_TOKEN;
+
+    const utbc = new UTBC.URLTokenBaseHTTPClient(token, ALGO_SERVER, port);
+    const rhc = new RHC.ReachHTTPClient(utbc, 'algodv2', async (e) => {
+        // Do nothing
+    });
+    return [rhc, new algosdk.Algodv2(rhc)];
+}
+
+/**
+ * This redefinition allows us to redefine `getProvider` properly, and it also allows us to make
+ * a provider synchronously (e.g. to make a default provider and reuse algod client and indexer in `AppContext`)
+ */
+export function makeProviderByEnv(env) {
+    const fullEnv = envDefaultsALGO(env);
+    const [algod_bc, algodClient] = algodClientFromEnv(fullEnv);
+    const [indexer_bc, indexer] = indexerFromEnv(fullEnv);
+    const isIsolatedNetwork = truthyEnv(fullEnv.REACH_ISOLATED_NETWORK);
+    const nodeWriteOnly = truthyEnv(fullEnv.ALGO_NODE_WRITE_ONLY);
+    const errmsg = (s) =>
+        `Providers created by environment ${s}. Calling setProviderByEnv or setProviderByName removes this capability. Try removing calls to those functions.`;
+
+    const getDefaultAddress = async () => {
+        throw new Error(errmsg(`do not have default addresses`));
+    };
+
+    const signAndPostTxns = async (txns, options) => {
+        void options;
+        const stxns = txns.map((txn) => {
+            if (txn.stxn) {
+                return txn.stxn;
+            }
+            throw new Error(errmsg(`cannot interactively sign`));
+        });
+        const bs = stxns.map((stxn) => Buffer.from(stxn, 'base64'));
+        await algodClient.sendRawTransaction(bs).do();
+    };
+
+    return {
+        algod_bc,
+        indexer_bc,
+        algodClient,
+        indexer,
+        nodeWriteOnly,
+        isIsolatedNetwork,
+        getDefaultAddress,
+        signAndPostTxns,
+    };
+}
+
+
 // Setting up (and remembering!) the reach stuff
 const reach = loadStdlib(process.env);
 const account = await reach.newAccountFromMnemonic(MNEMONIC);
-reach.setProviderByName(NETWORK);
+reach.setProvider(makeProviderByEnv(REACH_ALGO_ENV));
 
 const CONTRACT_PKGS = {
   crowdsale: {
@@ -96,7 +190,7 @@ export const fetchContractsGlobalViews = async ({
   if (!contractType in CONTRACT_PKGS) {
     throw new Error(`unknown contract type ${contractType}`);
   }
-
+  
   const results = await mapConcurrent(idVersions, async ({ id, version }) => {
     try {
       const { backend } = CONTRACT_PKGS[contractType][version];
