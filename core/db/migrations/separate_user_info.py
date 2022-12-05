550d0a5c42c9d172f2cd9119792cc62f48a089e1
wackloner <wackloner@gmail.com>
2022-12-05 15:08:06 +0800

    fix migration

--- core/db/migrations/separate_user_info.py
+++ core/db/migrations/separate_user_info.py
@@ -12,14 +12,14 @@ logger = logging.getLogger(__name__)
 def migrate():
     logger.info('Migrating users...')
 
-    users = get_db_collection(settings.db_name, 'users')
+    users = get_db_collection('COMETA_BOT', 'users')
     cometa_users = get_db_collection(settings.db_name, 'cometa_users')
     bot_users = get_db_collection(settings.db_name, 'bot_users')
 
     cnt = 0
     for user in users.find():
-        cometa_users.insert_one(CometaUser(user.address, user.pools).to_dict())
-        bot_users.insert_one(BotUser(user.address, user.telegram_id).to_dict())
+        cometa_users.insert_one(CometaUser(user['algo_address'], user['pools']).to_dict())
+        bot_users.insert_one(BotUser(user['algo_address'], user['telegram_id']).to_dict())
         cnt += 1
 
     logger.info(f'Migrated {cnt} users')
