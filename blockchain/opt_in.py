2a3ba77b206cc02037a832d82ddba0042a1a3c44
wackloner <wackloner@gmail.com>
2022-12-14 13:55:44 +0800

    mass opt-in script

---
+++ blockchain/opt_in.py
@@ -0,0 +1,36 @@
+from algosdk import mnemonic, account
+from algosdk.future import transaction
+
+from blockchain.node import init_algod_client
+from env import settings
+
+algod_client = init_algod_client()
+private_key = mnemonic.to_private_key(settings.algo_mnemonic)
+public_key = account.address_from_private_key(private_key)
+
+
+def opt_in(asa_ids: list[int]):
+    for asa_id in asa_ids:
+        params = algod_client.suggested_params()
+        txn = transaction.AssetTransferTxn(
+            sender=public_key,
+            sp=params,
+            receiver=public_key,
+            amt=0,
+            index=asa_id)
+
+        signed_txn = txn.sign(private_key)
+        txid = algod_client.send_transaction(signed_txn)
+
+        print(f'txid = {txid}')
+
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 3)
+
+
+asa_ids = [
+    534930853
+    # ADD YOURS
+]
+
+if __name__ == '__main__':
+    opt_in(asa_ids)
