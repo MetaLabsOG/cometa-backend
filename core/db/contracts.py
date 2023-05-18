affc20ac2fe7385ebd4eae2800c2e80d75bd1d7c
wackloner <wackloner@gmail.com>
2023-05-18 16:36:55 +0700

    fix python 3.10 syntax

--- core/db/contracts.py
+++ core/db/contracts.py
@@ -37,7 +37,7 @@ def get_all_contracts() -> list[ContractInfo]:
     return get_contracts({})
 
 
-def get_contracts_by_type(type: str | None) -> list[ContractInfo]:
+def get_contracts_by_type(type: Optional[str]) -> list[ContractInfo]:
     if type is None:
         return get_contracts({'type': {'$in': ['distribution', 'farm']}})
     return get_contracts({'type': type})
