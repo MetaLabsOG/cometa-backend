88690e9b81dd58cf722a33106940a0bd8e10698e
wackloner <wackloner@gmail.com>
2023-10-09 20:17:06 +0700

    clean API code

--- blockchain/node.py
+++ blockchain/node.py
@@ -1,6 +1,7 @@
 import json
+from base64 import b64decode
 
-import requests
+from algosdk import mnemonic, account, transaction
 from algosdk.v2client.algod import AlgodClient
 from cachetools import cached, TTLCache
 
@@ -56,3 +57,31 @@ def print_asset_holding(algodclient, account, assetid):
             print("Asset ID: {}".format(scrutinized_asset['asset-id']))
             print(json.dumps(scrutinized_asset, indent=4))
             break
+
+
+def try_rekeyed_transaction():
+    algod_client = init_algod_client()
+    params = algod_client.suggested_params()
+
+    private_key = mnemonic.to_private_key(settings.algo_mnemonic)
+    public_key = account.address_from_private_key(private_key)
+
+    rekeyed_private_key = mnemonic.to_private_key(settings.rekeyed_mnemonic)
+    rekeyed_public_key = account.address_from_private_key(rekeyed_private_key)
+
+    unsigned_txn = transaction.PaymentTxn(
+        sender=public_key,
+        sp=params,
+        receiver='METASWXOZB3CFFNWD6BDWU7CG5E42HNWFJZMM6IWR7MCT4P7NDW6755IMM',
+        amt=1000000,
+        note=b"Checking rekeyed txn.",
+    )
+    signed_txn = unsigned_txn.sign(rekeyed_private_key)
+    txid = algod_client.send_transaction(signed_txn)
+    print("Successfully submitted transaction with txID: {}".format(txid))
+
+    # wait for confirmation
+    txn_result = transaction.wait_for_confirmation(algod_client, txid, 4)
+
+    print(f"Transaction information: {json.dumps(txn_result, indent=4)}")
+    print(f"Decoded note: {b64decode(txn_result['txn']['txn']['note'])}")
