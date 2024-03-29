f9dff6c1b656e6b12f1a7b03bca5e2ea71bad7a5
wackloner <wackloner@gmail.com>
2024-03-29 18:56:14 +0800

    add lazy update of the date to the contracts api

--- api/migrations.py
+++ api/migrations.py
@@ -4,10 +4,66 @@ from datetime import datetime
 from blockchain.node import get_current_round
 from blockchain.util import date_from_block
 from core.db.contracts import get_all_pool_contracts, update_contract_with
+from core.db.model import ContractInfo
 from core.decorators import safe_async_method
 from core.util import parse_bignum
 
 logger = logging.getLogger(__name__)
+
+
+def update_contract_start_end_dates(contract: ContractInfo) -> ContractInfo | None:
+    start_time = datetime.now()
+    current_block = get_current_round()
+
+    metadata = contract.metadata
+    if metadata is None:
+        logger.warning(f'Contract has no metadata: {contract}')
+        return None
+
+    cache = metadata.get('cache')
+    if cache is None:
+        logger.warning(f'Contract has no cache: {contract}')
+        return None
+
+    logger.info(f'Updating pool {contract.id}...')
+
+    end_block = metadata.get('end_block')
+    if end_block is None:
+        end_block = parse_bignum(cache['initial']['endBlock'])
+        metadata['end_block'] = end_block
+        logger.info(f'Updated end block for {contract.id} to {end_block}')
+
+    begin_block = metadata.get('begin_block')
+    if begin_block is None:
+        begin_block = parse_bignum(cache['initial']['beginBlock'])
+        metadata['begin_block'] = begin_block
+        logger.info(f'Updated begin block for {contract.id} to {begin_block}')
+
+    if metadata.get('end_date') is None:
+        end_date = date_from_block(end_block, current_block, start_time)
+        metadata['end_date'] = end_date
+        contract.end_date = end_date
+        logger.info(f'Updated end date for {contract.id} to {end_date}')
+
+    if metadata.get('begin_date') is None:
+        begin_date = date_from_block(begin_block, current_block, start_time)
+        metadata['begin_date'] = begin_date
+        contract.begin_date = begin_date
+        logger.info(f'Updated begin date for {contract.id} to {begin_date}')
+
+    if metadata.get('lock_length_blocks') is None:
+        lock_length_blocks = parse_bignum(cache['initial']['lockLengthBlocks'])
+        metadata['lock_length_blocks'] = lock_length_blocks
+        logger.info(f'Updated lock length blocks for {contract.id} to {lock_length_blocks}')
+
+    update_contract_with(
+        contract_id=contract.id,
+        metadata=metadata,
+        begin_date=contract.begin_date,
+        end_date=contract.end_date
+    )
+
+    logger.info(f'Pool {contract.id} updated: {contract.begin_date} - {contract.end_date}.')
 
 
 @safe_async_method
@@ -25,53 +81,15 @@ async def update_pool_start_end_dates() -> None:
                 logger.info(f'Skipping pool {contract.id}...')
                 continue
 
-            metadata = contract.metadata
-            if metadata is None:
-                logger.warning(f'Contract has no metadata: {contract}')
-                continue
+            contract = update_contract_start_end_dates(contract)
 
-            cache = metadata.get('cache')
-            if cache is None:
-                logger.warning(f'Contract has no cache: {contract}')
-                continue
-
-            logger.info(f'Updating pool {contract.id}...')
-
-            end_block = metadata.get('end_block')
-            if end_block is None:
-                end_block = parse_bignum(cache['initial']['endBlock'])
-                metadata['end_block'] = end_block
-                logger.info(f'Updated end block for {contract.id} to {end_block}')
-
-            begin_block = metadata.get('begin_block')
-            if begin_block is None:
-                begin_block = parse_bignum(cache['initial']['beginBlock'])
-                metadata['begin_block'] = begin_block
-                logger.info(f'Updated begin block for {contract.id} to {begin_block}')
-
-            if metadata.get('end_date') is None:
-                end_date = date_from_block(end_block, current_block, start_time)
-                metadata['end_date'] = end_date
-                contract.end_date = end_date
-                logger.info(f'Updated end date for {contract.id} to {end_date}')
-
-            if metadata.get('begin_date') is None:
-                begin_date = date_from_block(begin_block, current_block, start_time)
-                metadata['begin_date'] = begin_date
-                contract.begin_date = begin_date
-                logger.info(f'Updated begin date for {contract.id} to {begin_date}')
-
-            if metadata.get('lock_length_blocks') is None:
-                lock_length_blocks = parse_bignum(cache['initial']['lockLengthBlocks'])
-                metadata['lock_length_blocks'] = lock_length_blocks
-                logger.info(f'Updated lock length blocks for {contract.id} to {lock_length_blocks}')
-
-            update_contract_with(
-                contract_id=contract.id,
-                metadata=metadata,
-                begin_date=contract.begin_date,
-                end_date=contract.end_date
-            )
+            if contract is not None:
+                update_contract_with(
+                    contract_id=contract.id,
+                    metadata=contract.metadata,
+                    begin_date=contract.begin_date,
+                    end_date=contract.end_date
+                )
 
             logger.info(f'Pool {contract.id} updated: {contract.begin_date} - {contract.end_date}.')
 
