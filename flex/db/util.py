f473f750422da9cd9346cd9da06639eb1195b211
wackloner <wackloner@gmail.com>
2024-04-01 07:12:05 +0800

    pool state remove initial rewards from total staked

--- flex/db/util.py
+++ flex/db/util.py
@@ -1,5 +1,6 @@
 import re
 import uuid
+from typing import Any
 
 
 def string_to_snake_case(s: str) -> str:
@@ -8,3 +9,27 @@ def string_to_snake_case(s: str) -> str:
 
 def get_uuid() -> str:
     return str(uuid.uuid4())
+
+
+def dict_get_nested_field(data: dict, *field_names: str) -> Any:
+    for field_name in field_names:
+        if field_name not in data:
+            return None
+        data = data[field_name]
+    return data
+
+
+def dict_set_nested_field_and_create(data: dict, value: Any, *field_names: str) -> Any:
+    for field_name in field_names[:-1]:
+        if field_name not in data:
+            data[field_name] = {}
+        data = data[field_name]
+    data[field_names[-1]] = value
+
+
+def dict_set_nested_field(data: dict, value: Any, *field_names: str) -> Any:
+    for field_name in field_names[:-1]:
+        if field_name not in data:
+            return None
+        data = data[field_name]
+    data[field_names[-1]] = value
