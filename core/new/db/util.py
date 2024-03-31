94a15ab3042ada2ebe3fe81dc77121d84f5e2d7e
wackloner <wackloner@gmail.com>
2024-04-01 02:48:05 +0800

    make pool_state DB API work

--- core/new/db/util.py
+++ core/new/db/util.py
@@ -1,5 +1,10 @@
 import re
+import uuid
 
 
 def string_to_snake_case(s: str) -> str:
     return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
+
+
+def get_uuid() -> str:
+    return str(uuid.uuid4())
