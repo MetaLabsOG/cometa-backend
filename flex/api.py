74dab0e072693df2398b65379fea922b87b82762
wackloner <wackloner@gmail.com>
2024-04-02 03:21:44 +0800

    reorder api methods

--- flex/api.py
+++ flex/api.py
@@ -8,11 +8,17 @@ from env import settings
 from flex import db
 from flex.data.contracts import all_contracts_to_pools
 from flex.data.costs import calculate_pool_state_cost
+from flex.data.pools import get_pool_info_by_id
 from flex.db.model import PoolStateInfo, PoolInfo, PoolType, UserStateInfo, PoolStateCost
 from flex.data.pool_state import update_pool_state, update_all_pool_states, update_user_state
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
+
+
+@router.get('/pool/', tags=['Pools 2.0'])
+async def get_pool_by_id(pool_id: int) -> PoolInfo:
+    return get_pool_info_by_id(pool_id)
 
 
 @router.get('/pool/state', tags=['Pools 2.0'])
@@ -21,13 +27,13 @@ async def get_pool_state(pool_id: int) -> PoolStateInfo:
     return updated_state.to_info()
 
 
-@router.get('/pools/state/', tags=['Pools 2.0'])
-async def get_pool_state() -> list[PoolStateInfo]:
-    updated_states = await update_all_pool_states()
-    return [state.to_info() for state in updated_states]
+@router.get('/pool/cost', tags=['Pools 2.0'])
+async def get_pool_state_cost_by_id(pool_id: int) -> PoolStateCost:
+    pool_state = await update_pool_state(pool_id)
+    return calculate_pool_state_cost(pool_state)
 
 
-@router.get('/pools/info', tags=['Pools 2.0'])
+@router.get('/pools/', tags=['Pools 2.0'])
 async def get_pools_by(type: PoolType = PoolType.ANY) -> list[PoolInfo]:
     if type == PoolType.FARMING:
         return [pool.to_info() for pool in db.farming_pools.get_all()]
@@ -37,7 +43,13 @@ async def get_pools_by(type: PoolType = PoolType.ANY) -> list[PoolInfo]:
         return [pool.to_info() for pool in db.staking_pools.get_all()] + [pool.to_info() for pool in db.farming_pools.get_all()]
 
 
-@router.post('/pools/user/info', tags=['Pools 2.0'])
+@router.post('/pools/state/', tags=['Pools 2.0'])
+async def get_pool_state() -> list[PoolStateInfo]:
+    updated_states = await update_all_pool_states()
+    return [state.to_info() for state in updated_states]
+
+
+@router.post('/pools/user/', tags=['Pools 2.0'])
 async def get_user_pool_states_by_address(address: str) -> UserStateInfo:
     user_state = await update_user_state(address)
     if user_state is None:
@@ -45,10 +57,15 @@ async def get_user_pool_states_by_address(address: str) -> UserStateInfo:
     return user_state.to_info()
 
 
-@router.get('/pool/cost', tags=['Pools 2.0'])
-async def get_pool_state_cost_by_id(pool_id: int) -> PoolStateCost:
-    pool_state = await update_pool_state(pool_id)
-    return calculate_pool_state_cost(pool_state)
+@router.post('/pools/user/cost', tags=['Pools 2.0'])
+async def get_user_pool_states_cost_by_address(address: str) -> dict:
+    user_state = await update_user_state(address)
+    if user_state is None:
+        raise HTTPException(404, f'User with address {address} is not found!')
+    return {
+        'info': user_state.to_info(),
+        'cost': user_state.calculate_cost(),
+    }
 
 
 @router.post('/pools/migrate', tags=['Pools 2.0'])
