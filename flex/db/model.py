19ef33b37ef706ef07f93ae344479191056c2cc3
wackloner <wackloner@gmail.com>
2024-04-01 13:04:40 +0800

    polish Pools API

--- flex/db/model.py
+++ flex/db/model.py
@@ -1,20 +1,53 @@
 from dataclasses import dataclass, field
 from datetime import datetime
+from enum import Enum
 
 from dataclasses_json import dataclass_json
 
-from flex.blockchain import AssetInfo
+from flex.blockchain import AssetInfo, ALGO_ASSET_INFO
 from flex.db.classes.base_entity import BaseEntity
 from flex.db.util import get_uuid
+
+
+class PoolType(str, Enum):
+    STAKING = 'staking'
+    FARMING = 'farming'
+    ANY = 'any'
+
+
+@dataclass_json
+@dataclass
+class PoolInfo:
+    id: int
+    type: PoolType
+    description: str
+    address: str
+
+    stake_token: AssetInfo
+    reward_token: AssetInfo
+
+    reward_amount_micros: int
+    reward_amount: float
+    algo_reward_amount_micros: int
+    algo_reward_amount: float
+
+    begin_block: int
+    end_block: int
+    lock_length_blocks: int
+
+    deploy_date: datetime
+    begin_date: datetime
+    end_date: datetime
 
 
 @dataclass_json
 @dataclass
 class StakingPool(BaseEntity['StakingPool']):
     description: str
+    address: str
+
     stake_token: AssetInfo
     reward_token: AssetInfo
-
     reward_amount_micros: int
     algo_reward_amount_micros: int
 
@@ -29,18 +62,40 @@ class StakingPool(BaseEntity['StakingPool']):
     id: int
     created: datetime = field(default_factory=datetime.now)
     updated: datetime = field(default_factory=datetime.now)
+
+    def to_info(self) -> PoolInfo:
+        return PoolInfo(
+            id=self.id,
+            type=PoolType.STAKING,
+            description=self.description,
+            address=self.address,
+            stake_token=self.stake_token,
+            reward_token=self.reward_token,
+            reward_amount_micros=self.reward_amount_micros,
+            reward_amount=self.stake_token.micros_to_amount(self.reward_amount_micros),
+            algo_reward_amount_micros=self.algo_reward_amount_micros,
+            algo_reward_amount=ALGO_ASSET_INFO.micros_to_amount(self.algo_reward_amount_micros),
+            begin_block=self.begin_block,
+            end_block=self.end_block,
+            lock_length_blocks=self.lock_length_blocks,
+            deploy_date=self.deploy_date,
+            begin_date=self.begin_date,
+            end_date=self.end_date
+        )
 
 
 @dataclass_json
 @dataclass
 class FarmingPool(BaseEntity['FarmingPool']):
-    dex_name: str
     description: str
+    address: str
+
+    stake_token: AssetInfo
+    reward_token: AssetInfo
 
     first_token: AssetInfo
     second_token: AssetInfo
-    lp_token: AssetInfo
-    reward_token: AssetInfo
+    dex_name: str
 
     reward_amount_micros: int
     algo_reward_amount_micros: int
@@ -56,6 +111,26 @@ class FarmingPool(BaseEntity['FarmingPool']):
     id: int
     created: datetime = field(default_factory=datetime.now)
     updated: datetime = field(default_factory=datetime.now)
+
+    def to_info(self) -> PoolInfo:
+        return PoolInfo(
+            id=self.id,
+            type=PoolType.FARMING,
+            description=self.description,
+            address=self.address,
+            stake_token=self.stake_token,
+            reward_token=self.reward_token,
+            reward_amount_micros=self.reward_amount_micros,
+            reward_amount=self.stake_token.micros_to_amount(self.reward_amount_micros),
+            algo_reward_amount_micros=self.algo_reward_amount_micros,
+            algo_reward_amount=ALGO_ASSET_INFO.micros_to_amount(self.algo_reward_amount_micros),
+            begin_block=self.begin_block,
+            end_block=self.end_block,
+            lock_length_blocks=self.lock_length_blocks,
+            deploy_date=self.deploy_date,
+            begin_date=self.begin_date,
+            end_date=self.end_date
+        )
 
 
 @dataclass_json
