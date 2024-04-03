ec8e1dc50fd965c987da0feb0e59d0c0373ddaf4
wackloner <wackloner@gmail.com>
2024-04-04 06:34:15 +0800

    introduce block sync

---
+++ flex/sync_pools.py
@@ -0,0 +1,132 @@
+import asyncio
+import logging
+
+from flex import db
+from flex.blockchain import get_current_round, indexer_client
+from flex.data.pool_state import update_all_pool_states, update_pools_with_transactions
+from flex.data.transactions import ASSET_TRANSFER_TX, APPLICATION_CALL_TX, PAYMENT_TX
+from flex.db.model import SyncState, PoolTransaction, SyncBlock
+
+logger = logging.getLogger(__name__)
+
+
+def find_transfer_transactions(txns: list[dict]) -> list[dict]:
+    transfer_transactions = []
+    for tx in txns:
+        if ASSET_TRANSFER_TX in tx:
+            # Stake
+            transfer_transactions.append(tx)
+
+        elif APPLICATION_CALL_TX in tx:
+            inner_txns = tx['inner-txns']
+
+            is_claim = False
+            for inner_tx in inner_txns:
+                if PAYMENT_TX in inner_tx:
+                    is_claim = True
+            if is_claim:
+                # TODO: save claim tx as well
+                continue
+
+            for inner_tx in inner_txns:
+                if ASSET_TRANSFER_TX in inner_tx:
+                    # Withdraw
+                    transfer_transactions.append(inner_tx)
+    return transfer_transactions
+
+
+def process_transactions(txns: list[dict]) -> list[PoolTransaction]:
+    transfer_transactions = find_transfer_transactions(txns)
+    pool_transactions = []
+    for tx in transfer_transactions:
+        txid = tx['id']
+        sender = tx['sender']
+        tx_asa_id = tx[ASSET_TRANSFER_TX]['asset-id']
+        receiver = tx[ASSET_TRANSFER_TX]['receiver']
+        amount = tx[ASSET_TRANSFER_TX]['amount']
+        confirmed_round = tx['confirmed-round']
+
+        # TODO: optimize lookup locally
+        withdraw_pool = db.pool_states.get_one(address=sender)
+        if withdraw_pool is not None:
+            if withdraw_pool.asa_id == tx_asa_id:
+                pool_tx = PoolTransaction(
+                    id=txid,
+                    pool_id=withdraw_pool.pool_id,
+                    user_address=receiver,
+                    pool_address=withdraw_pool.address,
+                    asa_id=tx_asa_id,
+                    delta_amount_micros=-amount,
+                    confirmed_round=confirmed_round
+                )
+                pool_transactions.append(pool_tx)
+
+        stake_pool = db.pool_states.get_one(address=receiver)
+        if stake_pool is not None:
+            if stake_pool.asa_id == tx_asa_id:
+                pool_tx = PoolTransaction(
+                    id=txid,
+                    pool_id=stake_pool.pool_id,
+                    user_address=sender,
+                    pool_address=stake_pool.address,
+                    asa_id=tx_asa_id,
+                    delta_amount_micros=amount,
+                    confirmed_round=confirmed_round
+                )
+                pool_transactions.append(pool_tx)
+
+    return pool_transactions
+
+
+def get_sync_state() -> SyncState:
+    sync_state = db.sync_states.get_one()
+    if sync_state is None:
+        sync_state = db.sync_states.create(SyncState())
+    return sync_state
+
+
+async def sync_pools_loop():
+    sync_state = get_sync_state()
+
+    if sync_state.last_round is None:
+        logger.info('Syncing pools for the first time.')
+        await update_all_pool_states()
+
+        current_round = get_current_round()
+        logger.info(f'Another, shorter loop, starting from round {current_round}')
+        await update_all_pool_states()
+
+        sync_state.last_round = current_round
+        logger.info(f'Pools synced up to round {current_round}.')
+
+    while True:
+        await asyncio.sleep(1)
+
+        next_round = sync_state.last_round + 1
+        logger.debug(f'Waiting for round {next_round}...')
+
+        try:
+            block_dict = indexer_client.block_info(round_num=next_round)
+        except Exception as e:
+            logger.error(f'{next_round} round not yet available: {e}')
+            continue
+
+        try:
+            raw_transactions = block_dict['transactions']
+            logger.debug(f'Processing {len(raw_transactions)} transactions from round {next_round}')
+
+            pool_transactions = process_transactions(raw_transactions)
+            _ = await update_pools_with_transactions(pool_transactions)
+
+            sync_state.last_round = next_round
+            db.sync_states.update(sync_state)
+            db.sync_blocks.create(
+                SyncBlock(
+                    round=next_round,
+                    timestamp=block_dict['timestamp'],
+                    pool_tx_ids=[tx.id for tx in pool_transactions],
+                )
+            )
+        except Exception as e:
+            logger.error(f'Error processing round {next_round}: {e}')
+            continue
