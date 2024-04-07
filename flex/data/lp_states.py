17f31bc3c69c8f31fadbc3322da023fe2d9c2448
wackloner <wackloner@gmail.com>
2024-04-08 02:06:03 +0800

    lp priced state api

--- flex/data/lp_states.py
+++ flex/data/lp_states.py
@@ -5,7 +5,7 @@ from dataclasses_json import dataclass_json
 from flex.blockchain.info import get_address_assets, get_address_assets_with_algo, get_asset_total_supply
 from flex.data.assets import get_asset
 from flex.data.tinyman import get_tinyman_pool_info
-from flex.data.vestige import DexProvider
+from flex.data.vestige import DexProvider, get_asset_price
 from flex.db.model.blockchain import LpToken
 
 
@@ -22,7 +22,46 @@ class LpState:
     asset1_reserve: float
     asset2_reserve: float
     issued_tokens: float
+
+    token_price: float
+    token_price_usd: float
     swap_fee_apr: float | None = None
+
+
+def create_lp_state_with_price(
+        id: int,
+        token_id: int,
+        asset1_id: int,
+        asset2_id: int,
+        dex_provider: str,
+        address: str,
+        asset1_reserve: float,
+        asset2_reserve: float,
+        issued_tokens: float,
+        swap_fee_apr: float | None = None
+) -> LpState:
+    if issued_tokens == 0:
+        token_price = 0
+        token_price_usd = 0
+    else:
+        asset1_price = get_asset_price(asset1_id)
+        token_price = asset1_price.algo * asset1_reserve * 2 / issued_tokens  # both reserves cost the same
+        token_price_usd = asset1_price.usd * asset1_reserve * 2 / issued_tokens
+
+    return LpState(
+        id=id,
+        token_id=token_id,
+        asset1_id=asset1_id,
+        asset2_id=asset2_id,
+        dex_provider=dex_provider,
+        address=address,
+        asset1_reserve=asset1_reserve,
+        asset2_reserve=asset2_reserve,
+        issued_tokens=issued_tokens,
+        swap_fee_apr=swap_fee_apr,
+        token_price=token_price,
+        token_price_usd=token_price_usd
+    )
 
 
 def lp_state_from_lp_balances(lp_token: LpToken) -> LpState:
@@ -42,7 +81,7 @@ def lp_state_from_lp_balances(lp_token: LpToken) -> LpState:
     lp_token_total_supply_micros = get_asset_total_supply(lp_token.id)
     issued_lp_tokens_micros = lp_token_total_supply_micros - lp_token_reserve_micros
 
-    return LpState(
+    return create_lp_state_with_price(
         id=lp_token.pool_id,
         token_id=lp_token.id,
         asset1_id=lp_token.asset1_id,
@@ -58,7 +97,7 @@ def lp_state_from_lp_balances(lp_token: LpToken) -> LpState:
 def fetch_lp_state_by_token(lp_token: LpToken) -> LpState:
     if lp_token.dex_provider == DexProvider.TINYMAN_V2:
         pool_info = get_tinyman_pool_info(lp_token.asset1_id, lp_token.asset2_id)
-        return LpState(
+        return create_lp_state_with_price(
             id=lp_token.pool_id,
             token_id=lp_token.id,
             asset1_id=lp_token.asset1_id,
@@ -67,7 +106,7 @@ def fetch_lp_state_by_token(lp_token: LpToken) -> LpState:
             address=lp_token.address,
             asset1_reserve=pool_info.asset1_reserve,
             asset2_reserve=pool_info.asset2_reserve,
-            total_tokens=pool_info.total_lp_tokens
+            issued_tokens=pool_info.total_lp_tokens
         )
 
     return lp_state_from_lp_balances(lp_token)
--- flex/data/vestige.py
+++ flex/data/vestige.py
@@ -1,4 +1,5 @@
 import logging
+from dataclasses import dataclass
 from enum import Enum
 
 import requests
@@ -20,6 +21,12 @@ class DexProvider(str, Enum):
     TINYMAN_V2 = 'T3'
 
 
+@dataclass
+class Price:
+    algo: float
+    usd: float
+
+
 @cached(cache=TTLCache(ttl=settings.algo_price_ttl, maxsize=1))
 def get_algo_price_usd() -> float:
     url = f'{BASE_URL}/currency/USD/price'
@@ -39,8 +46,25 @@ def get_asset_price_usd_not_cached(asset_id: int) -> float:
     url = f'{BASE_URL}/asset/{asset_id}/price'
     response = requests.get(url)
     data = response.json()
-    logger.debug(f'Request {url} got response: {data}')
 
     if 'USD' not in data:
         raise MetaError(f'Failed request {url}: code = {response.status_code}')
     return data['USD']
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=settings.asset_prices_ttl))
+def get_asset_price(asset_id: int) -> Price:
+    return get_asset_price_not_cached(asset_id)
+
+
+def get_asset_price_not_cached(asset_id: int) -> Price:
+    if asset_id == 0:
+        return Price(algo=1, usd=get_algo_price_usd())
+
+    url = f'{BASE_URL}/asset/{asset_id}/price'
+    response = requests.get(url)
+    data = response.json()
+
+    if 'USD' not in data:
+        raise MetaError(f'Failed request {url}: code = {response.status_code}')
+    return Price(algo=data['price'], usd=data['USD'])
