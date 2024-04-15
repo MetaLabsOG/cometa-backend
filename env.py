d7e05959cafcf09953b8d575a516bd8b265f11a0
wackloner <wackloner@gmail.com>
2024-04-15 15:49:00 +0800

    refactor env settings

--- env.py
+++ env.py
@@ -6,12 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     algo_network: str
     algo_mnemonic: str
-    cometa_algo_mnemonic: str
-    cometa_rekey_mnemonic: str
     rekeyed_mnemonic: str = None
-
-    farm_creation_fee: int
-    farm_flat_algo_creation_fee: int
 
     algod_address: str
     algod_token: str
@@ -19,14 +14,31 @@ class Settings(BaseSettings):
 
     server_port: int
     workers_num: int
+    api_password: str
+    migrate: bool = False
+
     mongodb_host: str
     mongodb_port: int
+    new_db_name: str = 'cometa-updated'
 
     logging_level: str = 'INFO'
     logging_format: str = '[%(asctime)s][%(levelname)s][%(filename)s] %(message)s'
     logging_date_format: str = '%I:%M:%S'
 
-    api_password: str
+    return_all_user_pools: bool = False
+
+    background_user_pools_update: bool = False
+    background_pools_update: bool = False
+
+    sync_new_pools: bool = True
+    sync_liquidity_pools: bool = False
+    update_contract_caches: bool = True
+    update_contracts_chunk_size: int = 10
+
+    telegram_bot_api_token: str
+    telegram_channel_id: int
+    discord_notify_webhook_url: Optional[str] = None
+
     block_time: float = 2.7
     old_pool_end_date_days_ago: int = 30
 
@@ -36,30 +48,12 @@ class Settings(BaseSettings):
     lp_token_prices_ttl: int = 60
     total_tvl_ttl: int
 
-    lottery_check_lock: bool = True
-
-    background_user_pools_update: bool = False
-    background_pools_update: bool = False
-    migrate: bool = False
-
-    update_contract_caches: bool = True
-    update_contracts_chunk_size: int = 10
-
-    telegram_bot_api_token: str
-    telegram_channel_id: int
-    discord_notify_webhook_url: Optional[str] = None
-
+    farm_creation_fee: int
+    farm_flat_algo_creation_fee: int
     enable_js: bool = True
-
     reach_no_warn: bool = False
     reach_connector_mode: str = 'ALGO'
     sync_humble_pools: int = 0
-
-    new_db_name: str = 'cometa-updated'
-    sync_new_pools: bool = True
-    sync_liquidity_pools: bool = False
-
-    return_all_user_pools: bool = False
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
