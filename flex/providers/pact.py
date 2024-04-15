3c6fdd3a69e033de483efd2a5d5a6f8fa3affa75
wackloner <wackloner@gmail.com>
2024-04-16 04:35:50 +0800

    fix pactsdk

--- flex/providers/pact.py
+++ flex/providers/pact.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 
-# import pactsdk
+import pactsdk
 
 from env import settings
 from flex.blockchain.base import algod_client
 
-# pact = pactsdk.PactClient(algod_client, network=settings.algo_network)
+pact = pactsdk.PactClient(algod_client, network=settings.algo_network)
 
 
 @dataclass
@@ -17,23 +17,23 @@ class PactPoolInfo:
     address: str
 
 
-# def get_pact_pool_info(asset1_id: int, asset2_id: int, lp_token_id: int) -> PactPoolInfo | None:
-#     asset1 = pact.fetch_asset(asset1_id)
-#     asset2 = pact.fetch_asset(asset2_id)
-#
-#     pools = pact.fetch_pools_by_assets(asset1, asset2)
-#     pool = None
-#     for p in pools:
-#         if p.liquidity_asset.index == lp_token_id:
-#             pool = p
-#             break
-#     if pool is None:
-#         return None
-#
-#     return PactPoolInfo(
-#         lp_token_id=pool.liquidity_asset.index,
-#         asset1_id=asset1_id,
-#         asset2_id=asset2_id,
-#         app_id=pool.app_id,
-#         address=pool.get_escrow_address(),
-#     )
+def get_pact_pool_info(asset1_id: int, asset2_id: int, lp_token_id: int) -> PactPoolInfo | None:
+    asset1 = pact.fetch_asset(asset1_id)
+    asset2 = pact.fetch_asset(asset2_id)
+
+    pools = pact.fetch_pools_by_assets(asset1, asset2)
+    pool = None
+    for p in pools:
+        if p.liquidity_asset.index == lp_token_id:
+            pool = p
+            break
+    if pool is None:
+        return None
+
+    return PactPoolInfo(
+        lp_token_id=pool.liquidity_asset.index,
+        asset1_id=asset1_id,
+        asset2_id=asset2_id,
+        app_id=pool.app_id,
+        address=pool.get_escrow_address(),
+    )
