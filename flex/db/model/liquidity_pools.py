82089d1651cd3302c0a81c902a5648b9e65836da
wackloner <wackloner@gmail.com>
2024-04-16 06:16:40 +0800

    lp states api full_info param

--- flex/db/model/liquidity_pools.py
+++ flex/db/model/liquidity_pools.py
@@ -8,6 +8,30 @@ from flex.db.classes.base_entity import BaseEntity
 
 @dataclass_json
 @dataclass
+class LpStateInfo:
+    id: int
+    token_id: int
+    asset1_id: int
+    asset2_id: int
+    dex_provider: str
+    address: str
+
+    # TODO: could not fit into MongoDB ???
+    asset1_reserve_micros: int
+    asset2_reserve_micros: int
+    issued_tokens_micros: int
+
+    last_updated_round: int
+
+    asset1_reserve: float | None = None
+    asset2_reserve: float | None = None
+    issued_tokens: float | None = None
+
+    swap_fee_apr: float | None = None
+
+
+@dataclass_json
+@dataclass
 class LpState(BaseEntity['LpState']):
     token_id: int
     asset1_id: int
@@ -26,6 +50,21 @@ class LpState(BaseEntity['LpState']):
 
     updated: datetime = field(default_factory=datetime.now)
     created: datetime = field(default_factory=datetime.now)
+
+    def to_info(self) -> LpStateInfo:
+        return LpStateInfo(
+            id=self.id,
+            token_id=self.token_id,
+            asset1_id=self.asset1_id,
+            asset2_id=self.asset2_id,
+            dex_provider=self.dex_provider,
+            address=self.address,
+            asset1_reserve_micros=self.asset1_reserve_micros,
+            asset2_reserve_micros=self.asset2_reserve_micros,
+            issued_tokens_micros=self.total_tokens_micros,
+            last_updated_round=self.last_updated_round,
+            swap_fee_apr=self.swap_fee_apr
+        )
 
 
 @dataclass_json
