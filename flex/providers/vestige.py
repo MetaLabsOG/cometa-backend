69f6b0b8df1ead4f165dd376b6c11ad4c9c166e4
wackloner <wackloner@gmail.com>
2024-04-16 04:09:43 +0800

    fix dex provider names

--- flex/providers/vestige.py
+++ flex/providers/vestige.py
@@ -6,6 +6,7 @@ import requests
 from cachetools import cached, TTLCache
 
 from env import settings
+from flex.db.model.blockchain import LpToken
 from flex.meta_error import MetaError
 
 BASE_URL = 'https://free-api.vestige.fi'
@@ -19,6 +20,24 @@ class DexProvider(str, Enum):
     PACT = 'PT'
     TINYMAN = 'T2'
     TINYMAN_V2 = 'T3'
+
+
+DEX_PROVIDER_BY_NAME = {
+    'humble': DexProvider.HUMBLE,
+    'pact': DexProvider.PACT,
+    'tinyman': DexProvider.TINYMAN_V2,
+    'tinymanold': DexProvider.TINYMAN,
+}
+
+DEX_PROVIDERS = list(DEX_PROVIDER_BY_NAME.values())
+
+
+def is_valid_dex_provider(dex_provider: str) -> bool:
+    return dex_provider in DEX_PROVIDERS
+
+
+def get_dex_tag_by_name(name: str) -> str:
+    return DEX_PROVIDER_BY_NAME.get(name.lower()) or name
 
 
 @dataclass
@@ -68,3 +87,21 @@ def get_asset_price_not_cached(asset_id: int) -> Price:
     if 'USD' not in data:
         raise MetaError(f'Failed request {url}: code = {response.status_code}')
     return Price(algo=data['price'], usd=data['USD'])
+
+
+def fetch_lp_token(lp_token_id: int, asset1_id: int, asset2_id: int, dex_provider: str) -> LpToken:
+    ref_id = asset2_id if asset1_id == 0 else asset1_id
+    url = f'{BASE_URL}/pools/{dex_provider}?assets=%5B{ref_id}%5D'
+    response = requests.get(url)
+    data = response.json()
+    for token_data in data:
+        if token_data['token_id'] == lp_token_id:
+            address = token_data['address']
+            return LpToken(
+                id=lp_token_id,
+                pool_id=token_data['id'],
+                asset1_id=asset1_id,
+                asset2_id=asset2_id,
+                dex_provider=dex_provider,
+                address=address,
+            )
