c20c644870f16b9dcf3c17115259d59d4fff70d6
wackloner <wackloner@gmail.com>
2024-04-16 19:23:35 +0800

    derive asset prices from pool reserves

--- flex/data/asset_prices.py
+++ flex/data/asset_prices.py
@@ -6,12 +6,12 @@ from env import settings
 from flex import db
 from flex.blockchain.info import get_current_round, ALGO_ASSET
 from flex.data.assets import micros_to_amount, get_asset_details
-from flex.data.lp_states import get_tinyman_pool_lp_state_by_asset_id, get_priced_lp_state_by_lp_token_id
+from flex.data.lp_states import get_tinyman_pool_lp_state_by_asset_id, get_priced_lp_state_by_lp_token_id, \
+    get_lp_state_by_lp_token
 from flex.data.lp_tokens import get_lp_token_by_id
 from flex.db.model.liquidity_pools import LpState
 from flex.db.model.priced import AssetPrice
 from flex.providers.vestige import get_full_asset_price, get_algo_price_usd
-from flex.sync_state import is_sync_delayed
 
 logger = logging.getLogger(__name__)
 
@@ -55,7 +55,8 @@ async def create_asset_price_from_tinyman_lp_state(lp_state: LpState, algo_price
 
 
 async def create_asset_price(asset_id: int, algo_price_usd: float | None = None) -> AssetPrice:
-    logger.debug(f'Creating asset price {asset_id}')
+    asset_details = await get_asset_details(asset_id)
+    logger.debug(f'Creating asset price {asset_id} = {asset_details.name}')
 
     lp_token = await get_lp_token_by_id(asset_id)
     if lp_token is not None:
@@ -63,31 +64,18 @@ async def create_asset_price(asset_id: int, algo_price_usd: float | None = None)
         priced_lp_state = await get_priced_lp_state_by_lp_token_id(lp_token.id)
         asset_price = AssetPrice(
             id=lp_token.id,
-            name=(await get_asset_details(lp_token.id)).name,
+            name=asset_details.name,
             price_algo=priced_lp_state.token_price_algo,
             price_usd=priced_lp_state.token_price_usd,
             last_update_round=priced_lp_state.last_updated_round
         )
-
     else:
-        lp_state = await get_tinyman_pool_lp_state_by_asset_id(asset_id)
-        if lp_state is not None:
-            logger.info(f'Price for {asset_id}: Tinyman pool {lp_state.asset1_id}/{lp_state.asset2_id}')
-            price_algo = await calculate_algo_price_from_tiny_algo_pool(lp_state)
-            algo_price_usd = algo_price_usd or (await get_algo_price_usd())
-            asset_price = AssetPrice(
-                id=asset_id,
-                name=(await get_asset_details(asset_id)).name,
-                price_algo=price_algo,
-                price_usd=price_algo * algo_price_usd,  # TODO: refactor to use USDC/ALGO pool
-                last_update_round=lp_state.last_updated_round,
-                tinyman_algo_pool_id=lp_state.id
-            )
-        else:
+        asset_price = await get_simple_asset_price_from_pools(asset_id, algo_price_usd)
+        if asset_price is None:
             price = await get_full_asset_price(asset_id)
             asset_price = AssetPrice(
                 id=asset_id,
-                name=(await get_asset_details(asset_id)).name,
+                name=asset_details.name,
                 price_algo=price.algo,
                 price_usd=price.usd,
                 last_update_round=await get_current_round()
@@ -95,6 +83,32 @@ async def create_asset_price(asset_id: int, algo_price_usd: float | None = None)
     db.asset_prices.create(asset_price)
     logger.info(f'\nNew Asset Price ${asset_price.name}: usd = {asset_price.price_usd}, algo = {asset_price.price_algo}\n')
     return asset_price
+
+
+async def get_simple_asset_price_from_pools(asset_id: int, algo_price_usd: float | None = None) -> AssetPrice | None:
+    tiny_lp_state = await get_tinyman_pool_lp_state_by_asset_id(asset_id)
+    if tiny_lp_state is not None:
+        price_algo = await calculate_algo_price_from_tiny_algo_pool(tiny_lp_state)
+        algo_price_usd = algo_price_usd or (await get_algo_price_usd())
+        asset_price = db.asset_prices.get_one(id=asset_id)
+        if asset_price is not None:
+            asset_price.price_algo = price_algo
+            asset_price.price_usd = price_algo * algo_price_usd
+            asset_price.last_update_round = tiny_lp_state.last_updated_round
+            db.asset_prices.update(asset_price)
+        else:
+            asset_price = AssetPrice(
+                id=asset_id,
+                name=(await get_asset_details(asset_id)).name,
+                price_algo=price_algo,
+                price_usd=price_algo * algo_price_usd,
+                last_update_round=tiny_lp_state.last_updated_round,
+                tinyman_algo_pool_id=tiny_lp_state.id
+            )
+            db.asset_prices.create(asset_price)
+        return asset_price
+
+    return None
 
 
 async def update_asset_price(asset_price: AssetPrice, algo_price_usd: float | None = None) -> AssetPrice:
@@ -145,7 +159,7 @@ async def get_asset_price(asset_id: int) -> AssetPrice:
     asset_price = db.asset_prices.get_one(id=asset_id)
     if asset_price is None:
         asset_price = await create_asset_price(asset_id)
-    elif (await is_sync_delayed()):
+    elif (await get_current_round()) - asset_price.last_update_round > settings.asset_prices_ttl:
         asset_price = await update_asset_price(asset_price)
     return asset_price
 
