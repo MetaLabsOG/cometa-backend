de05026e8a9ffcfd88ba7602902a8f22b83c131f
wackloner <wackloner@gmail.com>
2024-04-16 10:15:58 +0800

    asset info api

--- flex/data/assets.py
+++ flex/data/assets.py
@@ -1,28 +1,67 @@
+import logging
+
+from cachetools import cached, LRUCache, TTLCache
+
 from flex import db
 from flex.blockchain.info import fetch_asset
-from flex.db.model.blockchain import Asset, AssetInfo
+from flex.db.model.blockchain import Asset, AssetInfo, AssetDetails
 
-session_assets = {}
+logger = logging.getLogger(__name__)
 
 
-def get_asset(asset_id: int) -> Asset:
-    asset = session_assets.get(asset_id)
+@cached(cache=LRUCache(maxsize=1024))
+def get_full_asset(asset_id: int) -> Asset:
+    asset = db.assets.get_by_primary_key(asset_id, throw_ex=False)
     if asset is None:
-        asset = db.assets.get_by_primary_key(asset_id, throw_ex=False)
-        if asset is None:
-            asset = fetch_asset(asset_id)
-            db.assets.create(asset)
-        session_assets[asset_id] = asset
+        asset = fetch_asset(asset_id)
+        db.assets.create(asset)
+        logger.info(f'New Asset: {asset.to_details()}')
     return asset
 
 
+@cached(cache=LRUCache(maxsize=1024))
 def get_asset_info(asset_id: int) -> AssetInfo:
-    return get_asset(asset_id).to_info()
+    return get_full_asset(asset_id).to_info()
+
+
+@cached(cache=LRUCache(maxsize=1024))
+def get_asset_details(asset_id: int) -> AssetDetails:
+    return get_full_asset(asset_id).to_details()
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=30))
+def get_all_asset_details() -> list[AssetDetails]:
+    all_assets = db.assets.get_all()
+    return [asset.to_details() for asset in all_assets]
 
 
 def micros_to_amount(asset_id: int, amount_micros: int) -> float:
-    return get_asset(asset_id).micros_to_amount(amount_micros)
+    return get_full_asset(asset_id).micros_to_amount(amount_micros)
 
 
 def amount_to_micros(asset_id: int, amount: float) -> int:
-    return get_asset(asset_id).amount_to_micros(amount)
+    return get_full_asset(asset_id).amount_to_micros(amount)
+
+
+def load_all_assets_data() -> list[Asset]:
+    logger.info('Loading all assets data.')
+
+    asset_ids = {0}  # ALGO
+
+    for pool in db.farming_pools.get_all():
+        asset_ids.add(pool.stake_token.id)
+        asset_ids.add(pool.reward_token.id)
+        asset_ids.add(pool.first_token.id)
+        asset_ids.add(pool.second_token.id)
+
+    for pool in db.staking_pools.get_all():
+        asset_ids.add(pool.stake_token.id)
+        asset_ids.add(pool.reward_token.id)
+
+    assets = []
+    for asset_id in asset_ids:
+        asset = get_full_asset(asset_id)
+        assets.append(asset)
+
+    logger.info(f'{len(asset_ids)} assets data loaded.')
+    return assets
---
+++ flex/migrations/migrate_assets.py
@@ -0,0 +1,17 @@
+import logging
+
+from flex import db
+from flex.data.assets import load_all_assets_data
+
+
+logger = logging.getLogger(__name__)
+
+
+def asset_add_reserve() -> None:
+    logger.info('Adding reserve to assets')
+
+    removed_cnt = db.assets.clear()
+    logger.info(f'Removed {removed_cnt} assets')
+
+    asset_ids = load_all_assets_data()
+    logger.info(f'Loaded {len(asset_ids)} assets')
