b87df95fa92f23863fe27ba97824258db494612b
wackloner <wackloner@gmail.com>
2024-05-06 22:01:07 +0800

    pool fees api

--- flex/tdr_stats.py
+++ flex/tdr_stats.py
@@ -3,6 +3,9 @@ import json
 
 from flex import db
 from flex.blockchain.base import indexer_client
+from flex.data.asset_prices import get_asset_price
+from flex.data.pools import get_pools_by_query
+from flex.providers.vestige import vestige_full_asset_price_not_cached
 from flex.sync_pools import find_transfer_transactions, process_pool_transactions
 
 # 01.02 — 30.04
@@ -99,6 +102,50 @@ async def get_all_user_transactions(start_block: int, end_block: int) -> dict[st
             user_txns_by_pool_id.setdefault(user, {})[pool_id] = len(txns)
 
     return user_txns_by_pool_id
+
+
+ALGO_FEE = 100
+TOKEN_FEE_PERCENT = 0.01
+
+async def calculate_cometa_fees(start_block: int, end_block: int) -> dict:
+    pool_fee = {}
+    pool_infos = await get_pools_by_query({})
+    print(f'Calculate fees: in total {len(pool_infos)} pools')
+
+    total_algo = 0
+    total_token_fee_usd = 0
+    for pool in pool_infos:
+        if pool.begin_block > start_block and pool.begin_block < end_block:
+            print(f'Pool {pool.id}: {pool.reward_amount} {pool.reward_token.unit_name}')
+
+            reward_token_usd = (await vestige_full_asset_price_not_cached(pool.reward_token.id)).usd
+            token_fee = pool.reward_amount * TOKEN_FEE_PERCENT
+            token_fee_usd = token_fee * reward_token_usd
+            pool_fee[pool.id] = {
+                'token_fee': {
+                    'asa_id': pool.reward_token.id,
+                    'amount': token_fee,
+                    'amount_usd': token_fee_usd
+                },
+                'algo_fee': ALGO_FEE
+            }
+            total_algo += ALGO_FEE
+            total_token_fee_usd += token_fee_usd
+
+            print(f'{ALGO_FEE} ALGO, {token_fee} {pool.reward_token.unit_name}, {token_fee_usd} USD\n{pool.stake_token}\n{pool.reward_token}\nreward_amount: {pool.reward_amount}\n\n')
+
+    print(f'\n\n{len(pool_fee)} pools in period {DATE_0_BLOCK} — {DATE_1_BLOCK}: {total_algo} ALGO, {total_token_fee_usd} USD\n\n')
+
+    return pool_fee
+
+
+async def fetch_and_record_pool_fees() -> dict:
+    pool_fees = await calculate_cometa_fees(DATE_0_BLOCK, DATE_1_BLOCK)
+    with open(f'pool_fees_{DATE_0_BLOCK}_{DATE_1_BLOCK}.json', 'w') as f:
+        json.dump(pool_fees, f)
+
+    print(f'In total {len(pool_fees)} pools created in the period {DATE_0_BLOCK} - {DATE_1_BLOCK}')
+    return pool_fees
 
 
 async def fetch_and_record_user_txns() -> dict:
