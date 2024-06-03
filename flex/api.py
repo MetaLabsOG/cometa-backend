03d136a3336ea6e98645c9430cd4c6d6ad52bada
wackloner <wackloner@gmail.com>
2024-06-04 03:03:21 +0800

    fix encoding

--- flex/api.py
+++ flex/api.py
@@ -138,13 +138,14 @@ async def handle_get_lp_state_by_lp_token_id(lp_token_id: int) -> LpStateInfo:
 
 
 @router.post('/lp/state/priced', tags=['LP', 'Deprecated'])
-async def handle_get_lp_state_by_lp_token_id_DEPRECATED(lp_token_id: int) -> LpStateInfo:
-    async def fetch_lp_state_info():
+# async def handle_get_lp_state_by_lp_token_id_DEPRECATED(lp_token_id: int) -> LpStateInfo:
+async def handle_get_lp_state_by_lp_token_id_DEPRECATED(lp_token_id: int) -> dict:
+    async def fetch_lp_state_info_dict():
         lp_state = await get_lp_state_by_lp_token_id(lp_token_id)
         algo_price_usd = await get_algo_price_usd()
-        return lp_state.to_info(algo_price_usd)
+        return lp_state.to_info(algo_price_usd).to_dict()
 
-    return await global_cache_get(f'lp_state_info_{lp_token_id}', fetch_lp_state_info)
+    return await global_cache_get(f'lp_state_info_dict_{lp_token_id}', fetch_lp_state_info_dict)
 
 
 @router.post('/lp/states', tags=['LP'])
