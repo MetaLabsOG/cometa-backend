d1a2f0ebb05b03cf16976a4d120246f0f30a1714
wackloner <wackloner@gmail.com>
2026-03-14 01:23:58 +0700

    add MongoDB authentication support

--- core/db/mongodb.py
+++ core/db/mongodb.py
@@ -2,7 +2,12 @@ from pymongo import MongoClient
 
 from env import settings
 
-cluster = MongoClient(host=settings.mongodb_host, port=settings.mongodb_port)
+cluster = MongoClient(
+    host=settings.mongodb_host,
+    port=settings.mongodb_port,
+    username=settings.mongodb_username,
+    password=settings.mongodb_password,
+)
 
 
 def get_db_collection(db_name: str, name: str):
